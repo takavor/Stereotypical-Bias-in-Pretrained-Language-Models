eb1f3176351238d32f556e6e5ff6ba2a794d1dac
Sevag <baghsev@gmail.com>
2024-03-28 13:14:49 -0400

    create metrics file and update llama 2 evaluation file

--- llama2-evaluation.py
+++ llama2-evaluation.py
@@ -1,47 +1,23 @@
 import json
 
-# function to get lms on data as %
-def get_lms(data):
-    
-    meaningful_count = 0
-    for item in data:
-        if item['likelihoods']['predicted_label'] != 'unrelated':
-            meaningful_count += 1
-    
-    return meaningful_count / len(data) * 100
-
-# function to get ss on data as %
-def get_ss(data):
-    
-    ss_count = 0
-    total_count = 0
-    for item in data:
-        if item['likelihoods']['predicted_label'] == 'unrelated':
-            continue
-        if item['likelihoods']['predicted_label'] == 'stereotype':
-            ss_count += 1
-            
-        total_count += 1 
-
-    return ss_count / total_count * 100
-
-# function to get icat on data
-def get_icat(data):
-    lms = get_lms(data)
-    ss = get_ss(data)
-    icat = lms * min(ss, 100-ss) / 50
-    print(f'LMS: {lms}')
-    print(f'SS: {ss}')
-    print(f'iCat: {icat}')
-    return icat
-
 # load data
 with open('data/llama_preliminary_1.json') as file_1:
   data_1 = json.load(file_1)
   
 with open('data/llama_preliminary_2.json') as file_2:
   data_2 = json.load(file_2)
-  
-data1_icat = get_icat(data_1)
+
+
+from metrics import get_metrics
+lms_1, ss_1, icat_1 = get_metrics(data_1)
+lms_2, ss_2, icat_2 = get_metrics(data_2)
+
+print('Data 1')
+print(f'LMS: {lms_1}')
+print(f'SS: {ss_1}')
+print(f'iCAT: {icat_1}')
 print('---')
-data2_icat = get_icat(data_2)
+print('Data 2')
+print(f'LMS: {lms_2}')
+print(f'SS: {ss_2}')
+print(f'iCAT: {icat_2}')
